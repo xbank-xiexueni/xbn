b4a090def885e1edbb1056ef772e9d7d45e66a43
xbank-xiexueni <106423766+xbank-xiexueni@users.noreply.github.com>
2023-02-20 17:27:32 +0800

    chore: add build bash (#1)
    * chore: add build bash

---
+++ build.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+if [ "$CF_PAGES_BRANCH" == "production" ]; then  # Run the "production" script in `package.json` on the "production" branch  # "production" should be replaced with the name of your Production branch
+  yarn build:production
+
+else  # Else run the dev script  npm run devfi
+  yarn build:staging
+
+fi
