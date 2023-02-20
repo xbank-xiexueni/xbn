c1b76e5b149dbc476efb1f7ee177cd5537953954
xiexueni <xiexueni@xbank.plus>
2023-02-20 17:36:15 +0800

    chore: bash

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
 # !/bin/bash
-echo $CF_PAGES_BRANCH
 
-if [ "$CF_PAGES_BRANCH" == "production" ]; then  # Run the "production" script in `package.json` on the "production" branch  # "production" should be replaced with the name of your Production branch
-  echo 'production'
-  # yarn build:production
+if [ "$CF_PAGES_BRANCH" == "master" ]; then  # Run the "production" script in `package.json` on the "production" branch  # "production" should be replaced with the name of your Production branch
+  yarn build:production
 
 else  # Else run the dev script  npm run devfi
-  echo 'else'
-  # yarn build:staging
+  yarn build:staging
 
 fi
