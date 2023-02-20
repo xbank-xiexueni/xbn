22064440b841ebb42fea2de776ae13bcf68884d7
xiexueni <xiexueni@xbank.plus>
2023-02-20 17:21:16 +0800

    chore: add build bash

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 if [ "$CF_PAGES_BRANCH" == "production" ]; then  # Run the "production" script in `package.json` on the "production" branch  # "production" should be replaced with the name of your Production branch
   yarn build --mode production
 
-elif [ "$CF_PAGES_BRANCH" == "staging" ]; then  # Run the "staging" script in `package.json` on the "staging" branch  # "staging" should be replaced with the name of your specific branch
-  yarn build --mode staging
 else  # Else run the dev script  npm run devfi
+  yarn build --mode staging
+
